7402905db7d56dc7a8442dd5ebc599dd1c620d24
Dinesh Loganathan <dloganathan@ivp.in>
2024-09-29 19:26:12 +0530

    first commit

---
+++ SampleDotNetWithMultipleProj/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleDotNetWithMultipleProj;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+         Random r = new Random();
+        int random_number = r.Next(1, int.MaxValue);
+        Console.WriteLine("Finding factors for => {0}",random_number);
+        SampleLib.ExampleCommonClass.GetPrimeFactors(random_number);
+    }
+}
---
+++ SampleLib/ExampleCommonClass.cs
@@ -0,0 +1,32 @@
+﻿namespace SampleLib;
+
+public class ExampleCommonClass
+{
+    public static void GetPrimeFactors(int n=1)
+    {
+        List<int> factors = new List<int>();
+        while (n % 2 == 0)
+        {
+            factors.Add(2);
+            n /= 2;
+        }
+        for (int i = 3; i <= Math.Sqrt(n); i += 2)
+        {
+            while (n % i == 0)
+            {
+                factors.Add(i);
+                n /= i;
+            }
+        }
+        if (n > 2)
+        {
+            factors.Add(n);
+        }
+        Console.Write("\nFactors=>");
+        foreach(int i in factors)
+        {
+            Console.Write("\t{0}",i);
+        }
+
+    }
+}
